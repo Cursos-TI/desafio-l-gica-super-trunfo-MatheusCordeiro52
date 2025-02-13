feaa05f456bd5bd226facbdc3c3185ff1a13187d
MatheusCordeiro52 <matheuscamilo090@gmail.com>
2025-02-13 21:44:31 +0000

    COMMIT NIVEL MEST

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main() {
-   //variavel para o menu interativo
+//variavel para o menu interativo
    int menuInterativo;
-
-    //variaveis da carta1 e carta 2
+   int vitoriasCarta1 = 0, vitoriasCarta2 = 0;
+//variaveis da carta1 e carta 2
    char Estado[50], Estado2[50];
    char codigoCidade[3], codigoCidade2[3];
    char nomeCidade[50], nomeCidade2[50];
@@ -17,7 +17,7 @@ int main() {
    long int superPoder,superPoder2;
    char *comparacao;
 
-            //Entrada de dados carta 1
+//Entrada de dados carta 1
             printf("-=DADOS DA PRIMEIRA CARTA=-\n");
             printf("Digite o código do estado de A a H: \n"); 
             scanf(" %s", Estado);
@@ -35,14 +35,14 @@ int main() {
             scanf("%d", &pontoTuristico);
             printf("\n");
       
-            //Calculo de densidade populacional carta 1
+//Calculo de densidade populacional carta 1
          densidadePopulacional = ((float)populacao / area);
-         //Calculo de PIB per capita carta 1
+//Calculo de PIB per capita carta 1
          pibPerCapita = (pib / populacao);
-         //Calculo do Super Poder carta 1
+//Calculo do Super Poder carta 1
          superPoder = (populacao + (int)area + (int)pib + pontoTuristico + (int)densidadePopulacional + (int)pibPerCapita);
       
-               //Entrada de dados carta 2
+//Entrada de dados carta 2
             printf("-=DADOS DA SEGUNDA CARTA=-\n");
             printf("Digite o código do Estado de A a H: \n");
             scanf(" %s", Estado2);
@@ -60,14 +60,14 @@ int main() {
             scanf("%d", &pontoTuristico2);
             printf("\n");
 
-         //Calculo de densidade populacional carta 2
+//Calculo de densidade populacional carta 2
          densidadePopulacional2 = ((float)populacao2 / area2);
-         //Calculo de PIB per capita carta 2
+//Calculo de PIB per capita carta 2
          pibPerCapita2 = (pib2/ populacao2);
-         //Calculo do Super Poder carta 2
+//Calculo do Super Poder carta 2
          superPoder2 = (populacao2 + (int)area2 + (int)pib2 + pontoTuristico2 + (int)densidadePopulacional2 + (int)pibPerCapita2);
       
-         //saida com os dados e suas descrições carta 1
+//saida com os dados e suas descrições carta 1
          printf("-=DADOS PRIMEIRA CARTA=-\n");
          printf("CÓDIGO DA CARTA: %s%s\n", Estado, codigoCidade);
          printf("NOME DA CIDADE: %s\n", nomeCidade);
@@ -80,7 +80,7 @@ int main() {
          printf("SUPER PODER DA CARTA: %ld\n", superPoder);
          printf("\n"); 
 
-               //saida com os dados e suas descrições 2
+//saida com os dados e suas descrições 2
          printf("-=DADOS SEGUNDA CARTA=-\n");        
          printf("CÓDIGO DA CARTA: %s%s\n", Estado2, codigoCidade2);
          printf("NOME DA CIDADE: %s\n", nomeCidade2);
@@ -92,9 +92,9 @@ int main() {
          printf("PIB per capita: %.2f reais \n", pibPerCapita2);
          printf("SUPER PODER DA CARTA: %ld\n", superPoder2);
          printf("\n");
-         //saida de dados com disputa de cartas
+//função para código ficar em loop para o usuario não precisar reiniciar o código
          do {
-            //Menu interativo para escolha de comparações
+//Menu interativo para escolha de comparações
             printf("-= MENU PARA COMPARAÇÃO DE CARTAS =-.\n");
             printf("1. POPULAÇÃO.\n");
             printf("2. ÁREA.\n");
@@ -103,42 +103,58 @@ int main() {
             printf("5. DENSIDADE POPULACIONAL:\n");
             printf("6. PIB PER CAPITA:\n");
             printf("7. SUPER PODER:\n");
-            printf("8. SAIR:\n");
+            printf("8. CARTA GANHADORA:\n");
+            printf("9. SAIR:\n");
             scanf("%d", &menuInterativo);
-
+            
+//função para impressão de disputa das cartas com operador ternário
             switch (menuInterativo)
             {
-            case 1: comparacao = (populacao > populacao2) ? "CARTA 1 VENCEU!" : "Carta 2 VENCEU!";
-            printf("POPULAÇÃO: %s\n", comparacao);
+            
+            case 1: if (populacao > populacao2) {printf("CARTA 1 VENCEU!\n"),vitoriasCarta1++;
+            }else if (populacao == populacao2) {printf("EMPATE!\n"); //linha para empate
+            }else {printf("CARTA 2 VENCEU!\n"), vitoriasCarta2++;}
             break;
-            case 2: comparacao = (area > area2) ? "CARTA 1 VENCEU!" : "Carta 2 VENCEU!";
-            printf("ÁREA: %s\n", comparacao);
+            case 2: if (area > area2) {printf("CARTA 1 VENCEU!\n"),vitoriasCarta1++;
+            }else if (area == area2) {printf("EMPATE!\n");//linha para empate
+            }else {printf("CARTA 2 VENCEU!\n"), vitoriasCarta2++;}
+            
             break;
-            case 3: comparacao = (pib > pib2) ? "CARTA 1 VENCEU!" : "Carta 2 VENCEU!";
-            printf("PIB: %s\n", comparacao);
+            case 3: if (pib > pib2) {printf("CARTA 1 VENCEU!\n"),vitoriasCarta1++;
+            }else if (pib == pib2) {printf("EMPATE!\n");//linha para empate
+            }else {printf("CARTA 2 VENCEU!\n"), vitoriasCarta2++;}
+            
             break;
-            case 4: comparacao = (pontoTuristico > pontoTuristico2) ? "CARTA 1 VENCEU!" : "Carta 2 VENCEU!";
-            printf("PONTOS TURISTICOS: %s\n", comparacao);
+            case 4: if (pontoTuristico > pontoTuristico2) {printf("CARTA 1 VENCEU!\n"),vitoriasCarta1++;
+            }else if (pontoTuristico == pontoTuristico2) {printf("EMPATE!\n");//linha para empate
+            }else {printf("CARTA 2 VENCEU!\n"), vitoriasCarta2++;}
             break;
-            case 5:
-            comparacao = (densidadePopulacional > densidadePopulacional2) ? "CARTA 2 VENCEU!" : "Carta 1 VENCEU!";
-            printf("DENSIDADE POPULACIONAL: %s\n", comparacao);
+
+            case 5:if (densidadePopulacional > densidadePopulacional2) {printf("CARTA 1 VENCEU!\n"),vitoriasCarta1++;
+            }else if (densidadePopulacional == densidadePopulacional2) {printf("EMPATE!\n");//linha para empate
+            }else {printf("CARTA 2 VENCEU!\n"), vitoriasCarta2++;}
             break;
-            case 6:
-            comparacao = (pibPerCapita > pibPerCapita2) ? "CARTA 1 VENCEU!" : "Carta 2 VENCEU!";
-            printf("PIB PER CAPITA: %s\n", comparacao);
+
+            case 6: if (pibPerCapita > pibPerCapita2) {printf("CARTA 1 VENCEU!\n"),vitoriasCarta1++;
+            }else if (pibPerCapita2 == pibPerCapita2) {printf("EMPATE!\n");//linha para empate
+            }else {printf("CARTA 2 VENCEU!\n"), vitoriasCarta2++;}
             break;
-            case 7: 
-            comparacao = (superPoder > superPoder2) ? "CARTA 1 VENCEU!" : "Carta 2 VENCEU!";
-            printf("SUPER PODER: %s\n", comparacao);
+
+            case 7: if (superPoder > superPoder2) {printf("CARTA 1 VENCEU!\n"),vitoriasCarta1++;
+            }else if (superPoder == superPoder2) {printf("EMPATE!\n");//linha para empate
+            }else {printf("CARTA 2 VENCEU!\n"), vitoriasCarta2++;}
             break;
+
             case 8:
-            printf("SAINDO...");
+            printf ("\n-=CARTA GANHADORA=-\n");
+            if (vitoriasCarta1 > 4){ printf("CARTA 1 VENCEU O JOGO!!\n");
+            }else{ printf("CARTA 2 VENCEU O JOGO!!\n");}
             break;
+
             default:
-            printf("SAINDO...");
-      
-            }
+            printf("SAINDO...\n");
+         }
+//Fim da função de loop
          } while (menuInterativo != 8);
-         return 0;
+   return 0;
 }
